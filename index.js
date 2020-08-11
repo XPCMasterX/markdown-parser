0f618e5905f7a538ba6b9043b45ff091fc4b88fa
beet461 <rushvithanthagiri@gmail.com>
2020-08-11 11:49:01 +0000

    Get master branch and merger

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 /* This file is to test markdown-parser */
 import {Mparser} from './modules/mparser/mparser.mjs'
 
-const parser1 = new Mparser("Hello *heloo* *heea* isdif *heaa* _hey_", ["<p>", "</p>"]);
+const parser1 = new Mparser("Hello *heloo*", ["<p>", "</p>"]);
 parser1.matchBold();
-console.log(parser1.parsedObject);
--- modules/mparser/mparser.mjs
+++ modules/mparser/mparser.mjs
@@ -41,27 +41,21 @@ class Mparser {
 
     matchBold() {
         for (var i = 0; i < this.markdownTrimmed.length; i++) {
-            this.hasMatchedRegex = false;
-
-            // Need to work on this
-            /*for (var j = 0; j < this.regexps.length; j++) {
-                if (this.markdownTrimmed[i].match(this.regexps[j].regex) != null && !this.hasMatchedRegex) {
-                    this.string = this.markdownTrimmed[i];
-                    this.formatted = this.string.slice(0, -1).substr(1);
-
-                    this.parsedObject[i] = {};
-                    this.parsedObject[i].content = this.formatted;
-                    this.parsedObject[i].htmlTags = this.regexps[j].tags;
+            /*this.hasMatchedRegex = false;
+            for (var j = 0; i < this.markdownTrimmed.length; j++) {
+                if (this.markdownTrimmed[i].match(this.regexps[j].regex) !== null && !this.hasMatchedRegex) {
+                    this.parsedObject.push(this.markdownTrimmed[i]);
+                    this.parsedObject[i].type = this.regexps[j].type;
+                    this.parsedObject[i].tags = this.regexps[j].tags;
                     this.hasMatchedRegex = true;
-                    console.log(this.parsedObject[i]);
-                } 
+                }
             }
-            if (this.hasMatchedRegex !== true) {
-                this.parsedObject[i] = {};
-                this.parsedObject[i].content = this.formatted;
-                this.parsedObject[i].htmlTags = this.defaultHTMLTag;
-                console.log(this.parsedObject[i] + "Failed");
+            if (!this.hasMatchedRegex) {
+                this.parsedObject.push(this.markdownTrimmed[i]);
+                this.parsedObject[i].type = "text";
+                this.parsedObject[i].tags = this.defaultHTMLTag;
             }*/
+            console.log(this.regexps.length)
         }
     }
 }
